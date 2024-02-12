ee6a6a170bb5287d70dda34ba79f9964b30d5437
Daniel Susetioe <dsusetio87@gmail.com>
2024-02-12 21:21:18 +0100

    iteration 1-4

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import "./App.css";
+import foodsJson from "./foods.json";
+import React, { useState } from 'react';
+import FoodBox from "./components/FoodBox";
+
+
+
 
 function App() {
+
+  const [foods, setFoods] = useState(foodsJson);
+
+
   return (
     <div className="App">
       <h1>LAB | React IronNutrition</h1>
+  
+      {foods.map((food) => (
+        <FoodBox key={food.id} food={food} />
+      ))}
     </div>
   );
 }
